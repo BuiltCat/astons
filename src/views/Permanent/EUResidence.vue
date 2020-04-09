a670a7e2012a7c60b6b6180f8b0549d71bbcdb9a
xiangbajiang <xiangbajiang@gmail.com>
2020-04-10 00:35:45 +0800

    px转rem，修改UK

--- src/views/Permanent/EUResidence.vue
+++ src/views/Permanent/EUResidence.vue
@@ -3,16 +3,16 @@
         <div class="page-container">
             <div class="page-container-content">
                 <ul class="page-header">
-                    <li>Astons</li>
-                    <li>Residency by investment</li>
+                    <li><a href="">Astons</a></li>
+                    <li><a href="">Residency by investment</a></li>
                 </ul>
             </div>
         </div>
         <div class="page-container">
-            <div class="page-container-content">
+            <div class="page-container-content content-padding">
                 <div class="title-swaper">
                     <div class="title-content">
-                        <h1>投资拘留</h1>
+                        <h1>投资居留</h1>
                         <p></p>
                         <div>
                             无论您渴望在欧州安顿，扩张事业或者免签证到申根国家旅游。Astons 将根据您所选择的国家提供专业的解决方案，让您在3-6个月内顺利替全家人取得欧洲的居留权。我们拥有良好的纪录，保证提供可信的建议，满足客户从初始谘询，申请居留许可甚至到取得公民身份的期待。马上联络我们开始您的快速取得欧洲居留权的道路。
@@ -22,7 +22,7 @@
                 </div>
             </div>
         </div>
-        <div class="page-container">
+        <div class="page-container landing-citizenship--white">
             <div class="page-container-content">
                 <div class="list-content">
                     <h2>欧盟居留权</h2>
@@ -76,8 +76,8 @@
             </div>
         </div>
         <div class="page-container">
-            <div class="page-container-content">
-                <div>
+            <div class="page-container-content landing-section__wrap">
+                <div class="EU-footer">
                     <a class="title-btn" href="">请联系我</a>
                 </div>
             </div>
@@ -91,32 +91,58 @@
 </script>
 <style lang="css">
     .page-content{
-        margin-top: 120px;
+        margin-top: 10rem;
+    }
+    .page-content ul{
+        list-style: none;
+    }
+    .page-content a{
+        text-decoration: none;
     }
     .page-container{
         width: 100%;
-        margin-bottom: 20px;
     }
     .page-container-content{
-        margin: 0 311.5px;
-        padding: 0 66px;
+        padding-right: 5.5rem;
+        padding-left: 5.5rem;
+        max-width: 1280px;
+        margin-right: auto;
+        margin-left: auto;
+    }
+    .content-padding{
+        padding-top: 1rem;
+        padding-bottom: 8rem;
     }
     .page-header{
-        font-size: 12px;
+        font-size: 1rem;
         list-style: none;
         display: flex;
         color: #B2B2B2;
+        padding: 0 0.8rem 1.6rem 0.5rem;
     }
+    .page-header a{
+        font-size: 1.5rem;
+        line-height: 2rem;
+        text-decoration: none;
+        transition: all 0.2s;
+        color: #B2B2B2;
+        font-size: 1.2rem;
+        line-height: 1.6rem;
+        display: inline-block;
+        padding: 0 0.8rem 1.6rem 0rem;
+        position: relative;
+    }
+
     .title-swaper{
         display: flex;
     }
     .title-content{
-        padding: 0px 10px;
+        padding: 0px 0px;
         width: 66.6667%;
     }
     .title-content h1{
-        font-size: 50px;
-        margin-bottom: 18px;
+        font-size: 5rem;
+        line-height: 5.5rem;
     }
     .title-content p{
         font-size: 15px;
@@ -144,7 +170,6 @@
         overflow: hidden;
         margin: 0 0 0 auto;
         text-decoration: none;
-
     }
     .title-btn:hover, .title-btn:active {
         color: #fff;
@@ -152,19 +177,18 @@
         background-color: #CC0026;
         text-decoration: none;
     }
-
-    .list-content{
-        padding: 0px 0px;
+    .landing-citizenship--white {
+        padding-top: 0;
+        padding-bottom: 0;
+        background-color: #fff;
     }
     .list-content h2{
-        font-size: 36px;
-        padding: 48px 0px;
+        font-size: 3.6rem;
+        line-height: 4.4rem;
+        padding: 4rem 0;
+        border-top: 1px solid #E5E5E5;
     }
-    .list-content ul{
-        list-style: none;
-        margin: 0;
-        padding: 0;
-    }
+
     .list-content a{
         font-weight: bold;
         display: block;
@@ -176,23 +200,42 @@
         text-decoration: none;
     }
     .list-content .country-list{
+        margin-right: -1.5rem;
+        margin-left: -1.5rem;
         display: flex;
-        flex-wrap: wrap;
-        width: 100%;
+        justify-content: flex-start;
+        align-items: flex-start;
+        flex-flow: row wrap;
     }
     .list-content .country-list .country-item{
-        width: 33.33%;
-        margin: 0 0 72px;
-        padding: 0 18px;
+        width: 33.3333333333%;
+        padding-right: 1.5rem;
+        padding-left: 1.5rem;
+        margin-bottom: 6rem;
     }
     .list-content .country-list .country-item img{
         display: block;
+        height: 100%;
         width: 100%;
+        object-fit: cover;
         margin-bottom: 15px;
         position: relative;
-    }     
+    }
+    .list-content .country-list .country-item a{
+        font-size: 2.7rem;
+        line-height: 3.3rem;
+    }
     .list-content .country-list .country-item ul{
         font-size: 15px;
+    }
+    .EU-footer{
+        display: flex;
+        align-items: flex-end;
+    }
+    .landing-section__wrap {
+        padding-right: 5.5rem;
+        padding-left: 5.5rem;
+        padding-bottom: 8rem;
     }
     
 
--- src/views/Permanent/UKResidence.vue
+++ src/views/Permanent/UKResidence.vue
@@ -2,20 +2,16 @@
     <main class="page-content">
         <div class="page-container">
             <div class="page-container-content">
-                <div class="page-header">
-                    <ul>
-                        <li>Astons</li>
-                        <li>Citizenship by investment</li>
-                        <li>UK Residency & Citizenship by Investment</li>
+                <div>
+                    <ul class="page-header">
+                        <li><a herf="#">Astons</a></li>
+                        <li><a herf="#">Citizenship by investment</a></li>
+                        <li><a herf="#">UK Residency & Citizenship by Investment</a></li>
                     </ul>
                 </div>
-                <div>
-
-                </div>
-                This is a UKResidence page.
             </div>
         </div>
-        <div class="page-container">
+        <div class="page-container UK-margin">
             <div class="page-container-content">
                 <div>
                     <div class="UK-slogan">
@@ -23,65 +19,210 @@
                         <p class="landing-menu__desc">
                             英国投资者移民计划为希望在英国获得居留权并开启通往英国公民旅程的投资者及其家人，提供了最有效和安全的途径。专业的Tier 1投资者签证申请指导
                         </p>
-                        <svg class="landing-menu__bg" fill="none" height="120" viewBox="0 0 120 120" width="120" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M70.924 8.55927C68.0943 9.8122 65.3069 11.1496 62.5758 12.6137L69.4881 32.6888C54.2558 41.5297 41.3886 54.411 32.5617 69.6573L12.4866 62.7451C11.0366 65.4621 9.69922 68.2495 8.44629 71.0933L102.726 103.571C103.05 103.247 103.092 103.205 103.416 102.881L70.924 8.55927ZM72.557 41.5719L88.9296 89.0848L41.4308 72.7122C49.1173 60.0421 59.901 49.2585 72.557 41.5719Z" fill="white"></path>
-                            <path d="M0 99.1224L60.8165 120.084H87.5927C87.5927 119.958 87.5927 119.831 87.5927 119.578L1.90052 90.0421C1.14031 93.0266 0.520882 96.0675 0 99.1224Z" fill="white"></path>
-                            <path d="M120 60.9009L99.0383 0.0843506C95.9693 0.619311 92.9425 1.23874 89.958 1.99894L119.479 87.6911C119.733 87.6911 119.859 87.6911 120 87.6911V60.9009Z" fill="white"></path>
+                        <svg class="landing-menu__bg" fill="none" height="120" viewBox="0 0 120 120" width="120"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M70.924 8.55927C68.0943 9.8122 65.3069 11.1496 62.5758 12.6137L69.4881 32.6888C54.2558 41.5297 41.3886 54.411 32.5617 69.6573L12.4866 62.7451C11.0366 65.4621 9.69922 68.2495 8.44629 71.0933L102.726 103.571C103.05 103.247 103.092 103.205 103.416 102.881L70.924 8.55927ZM72.557 41.5719L88.9296 89.0848L41.4308 72.7122C49.1173 60.0421 59.901 49.2585 72.557 41.5719Z"
+                                fill="white"></path>
+                            <path
+                                d="M0 99.1224L60.8165 120.084H87.5927C87.5927 119.958 87.5927 119.831 87.5927 119.578L1.90052 90.0421C1.14031 93.0266 0.520882 96.0675 0 99.1224Z"
+                                fill="white"></path>
+                            <path
+                                d="M120 60.9009L99.0383 0.0843506C95.9693 0.619311 92.9425 1.23874 89.958 1.99894L119.479 87.6911C119.733 87.6911 119.859 87.6911 120 87.6911V60.9009Z"
+                                fill="white"></path>
                         </svg>
                     </div>
                     <ul class="UK-link">
-                        <li>内容</li>
-                        <li>Routes</li>
-                        <li>Eligibility</li>
-                        <li>計畫優點</li>
-                        <li>免簽證旅遊</li>
-                        <li>取得公民身份流程</li>
+                        <li><a herf="#">内容</a></li>
+                        <li><a herf="#">Routes</a></li>
+                        <li><a href="#">Eligibility</a></li>
+                        <li><a href="#">計畫優點</a></li>
+                        <li><a href="#">免簽證旅遊</a></li>
+                        <li><a href="#">取得公民身份流程</a></li>
                     </ul>
                 </div>
+            </div>
+        </div>
+        <div class="page-container UK-margin2">
+            <div class="page-container-content">
+                <div class="UK-routes-title">
+                    <h2>TIER 1投资者签证</h2>
+                    <P>-</P>
+                </div>
+                <div class="UK-routes-desc">
+                    <div class="UK-routes-desc-item">
+                        <div class="UK-routes-desc-text">
+                            <p>
+                                Tier
+                                1（投资者）签证为投资者申请人及其家人开启了一条安全快捷的申请通道。签证可以让他们移居到英国，享受高水平、安全有保障的生活，子女接收良好的教育，并同时走上成为英国公民的旅程。投资路径要求对活跃的英国企业进行200万英镑/
+                                500万英镑或1000万英镑的股票或公司债券进行大量投资。
+                            </p>
+                        </div>
+                    </div>
+                    <div class="UK-routes-desc-item">
+                        <div class="UK-routes-desc-text">
+                            <p>
+                                Tier
+                                1申请是快速的：处理时间只需3-4周。初始签证的有效期为3年4个月（如果在英国境内申请，有效期为3年）。初始签证到期后，只要符合申请要求，申请人可以获得有效期为2年的续签。在英国连续住满5年的申请人可以申请永久居留（ILR）投资额为500万英镑可以在首签3年后，或投资额为1000万英镑可以在首签2年后直接申请永久居留。申请初始签证时无英文语言要求，申请ILR永久居留时需考察
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <table class="UK-routes-table">
+                    <tbody>
+                        <tr>
+                            <th colspan="4">TIER 1_投资者_基于投资额的安置途径</th>
+                        </tr>
+                        <tr>
+                            <td>200万英镑</td>
+                            <td>500万英镑</td>
+                            <td>1000万英镑</td>
+                        </tr>
+                        <tr>
+                            <td>5年后可申请永久居留</td>
+                            <td>3年后可申请永久居留</td>
+                            <td>2年后可申可申请永久居留</td>
+                        </tr>
+                        <tr>
+                            <td>6年后可申请公民身份</td>
+                            <td>5年后可申请公民身份</td>
+                            <td>5年后可申请公民身份</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </main>
 </template>
 <script>
-    export default{
+    export default {
 
     }
 </script>
 <style lang="css">
-    .page-content{
-        margin-top: 120px;
+    .UK-margin {
+        margin-top: 3rem;
     }
-    .page-content ul{
-        list-style: none;
+
+    .UK-margin2 {
+        padding-top: 8rem;
+        padding-bottom: 8rem;
     }
-    .page-container{
-        width: 100%;
-        margin-bottom: 20px;
-    }
-    .page-container-content{
-        margin: 0 311.5px;
-        padding: 0 66px;
-    }
-    .UK-slogan{
-        padding: 30px 24px;
+
+    .UK-slogan {
+        padding: 2.5rem 2rem 2.5rem 2rem;
         border-left: 3px solid #E4002B;
         background-color: #F2F2F2;
         position: relative;
     }
-    .UK-slogan h2{
-        font-size: 36px;
-        margin-bottom: 12px;
+
+    .UK-slogan h2 {
+        font-size: 3.6rem;
+        line-height: 4.4rem;
+        margin-bottom: 1rem
     }
-    .UK-slogan p{
-        font-size: 18px;
-        line-height: 27px;
+
+    .UK-slogan p {
+        font-size: 1.8rem;
+        line-height: 2.7rem;
         position: relative;
-        padding: 10px 0;
+        padding: .9rem 0;
         z-index: 1;
     }
-    .UK-slogan svg{
+
+    .UK-slogan svg {
         position: absolute;
         right: 0;
         bottom: 0;
     }
+
+    .UK-link {
+        display: flex;
+        justify-content: space-between;
+        flex-direction: column;
+        border-bottom: 1px solid #E5E5E5;
+        padding: 2.5rem 0 2rem 0;
+        flex-direction: row;
+    }
+
+    .UK-link a {
+        font-size: 1.2rem;
+        line-height: 1.4rem;
+        text-transform: uppercase;
+        font-weight: bold;
+        letter-spacing: 0.15rem;
+        color: #B2B2B2;
+        padding: 1.5rem 0;
+    }
+
+    .UK-routes-title {
+        margin-right: -1.5rem;
+        margin-left: -1.5rem;
+        display: flex;
+        justify-content: flex-start;
+        align-items: normal;
+        flex-flow: row wrap;
+    }
+
+    .UK-routes-title h2 {
+        font-size: 5rem;
+        line-height: 5.5rem;
+        margin-bottom: 0;
+    }
+
+    .UK-routes-title p {
+        width: 50%;
+        padding: 0 1.5rem;
+        font-size: 1.8rem;
+        line-height: 2.7rem;
+    }
+
+    .UK-routes-desc {
+        margin-right: -1.5rem;
+        margin-left: -1.5rem;
+        display: flex;
+        justify-content: flex-start;
+        align-items: normal;
+        flex-flow: row wrap;
+        font-size: 1.8rem;
+        line-height: 2.7rem;
+    }
+
+    .UK-routes-desc-item {
+        margin: 5rem 0;
+        width: 50%;
+        padding: 0 1.5rem;
+    }
+
+    .UK-routes-desc-text p {
+        padding: 0.9rem 0;
+    }
+
+    .UK-routes-table {
+        display: table;
+        text-align: left;
+        border-collapse: collapse;
+        width: 100%;
+    }
+    .UK-routes-table th {
+        font-size: 2.7rem;
+        line-height: 3.3rem;
+        font-weight: bold;
+        padding: 1.6rem 0;
+    }
+    .UK-routes-table tr:nth-child(even) {
+        background-color: #F2F2F2;
+    }
+    .UK-routes-table td {
+        font-size: 1.5rem;
+        line-height: 2rem;
+        padding: 1.6rem 1rem;
+    }
+    .UK-routes-table tr:nth-child(2) td {
+        font-size: 2.7rem;
+        line-height: 3.3rem;
+        font-weight: bold;
+        color: #E4002B;
+    }
+
 </style>
