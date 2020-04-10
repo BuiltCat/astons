d90197d2246a4b249b2b54562c0061d955f85e25
xiangbajiang <xiangbajiang@gmail.com>
2020-04-10 16:02:23 +0800

    护照投资移民项目

---
+++ src/views/Passport/index.vue
@@ -0,0 +1,350 @@
+<template>
+    <main class="page-content">
+        <div class="page-container">
+            <div class="page-container-content">
+                <ul class="page-header">
+                    <li><a href="#">Astons</a></li>
+                    <li><a href="#">Citizenship by investment</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="page-container">
+            <div class="page-container-content Passport-padding">
+                <div class="Passport-title">
+                    <div class="Passport-title-content">
+                        <h1>通过投资获得公民身份</h1>
+                        <div class="">
+                            <p>拥有第二本护照可能是你做过的最好的投资，可以增加全球流动性和个人安全、商业优惠和免税。查看我们的顶级公民项目，通过房地产投资或捐赠途径获得第二本护照。</p>
+                            <p><br></p>
+                            <p>如果你想在欧洲居住和生活，Astons的欧洲公民投资计划提供塞浦路斯，马耳他，英国以及保加利亚和黑山的目的地选择。希腊、西班牙、葡萄牙和许多其他国家也有提供欧洲居留许可的居留项目，未来也可进一步获得公民身份。</p>
+                            <p><br></p>
+                            <p><br>对于那些主要目标是获得全球流动性和免签进入欧洲的投资者来说，加勒比海和瓦努阿图的公民身份项目可能是一个更实惠的选择。通过最低10万美元的投资，在短短1-2个月内获得可免签前往150多个国家的第二本护照。</p>
+                            <p><br></p>
+                            <p>根据国家的不同，投资价值从刚刚超过10万美元(安提瓜和巴布达，圣卢西亚和多米尼克)到超过250万欧元(塞浦路斯)不等。投资范围选择广泛，包括捐赠政府基金、房地产投资或商业投资。Astons值得信赖的顾问将帮助您选择最佳的选择和公民项目，并根据您的需求提供具有吸引力的投资组合选择。</p>
+                            <p><br></p>
+                            <p>如果您正在寻找获得欧洲或加勒比公民身份的最快捷和最具成本效益的方法，请今天与Astons的专家预约免费咨询，以获得适合您具体需求的合格建议。</p>
+                            <p><br></p>
+                            <p>Astons为客户获得居住权和公民身份提供一站式服务。整个过程由行业专家精心管理。这一过程始于一次彻底的磋商，讨论潜在的投资路线，并寻找我们最好的房地产项目。它会一直持续到你准备并提交完整的最终申请。Astons一直在你身边工作，直到你成为全球公民的那一刻</p>
+                        </div>
+                    </div>
+                    <a class="title-btn" href="#">请联系我</a>
+                </div>
+            </div>
+        </div>
+        <div class="page-container">
+            <div class="page-container-content">
+                <div class="Passport-country">
+                    <h2>欧洲公民身份</h2>
+                    <ul class="Passport-country-list">
+                        <li class="Passport-country-item">
+                            <a class="Passport-country-item-img" href="">
+                                <img src="../../assets/img/Passport/uk.jpg" alt="">
+                            </a>
+                            <a class="Passport-country-item-title" href="">英国</a>
+                            <ul>
+                                <li>4 个月登陆- 2年拿绿卡</li>
+                                <li>
+                                    <span class="Passport-country-desc-title">最低投资额</span>
+                                    &nbsp;-&nbsp;
+                                    <span class="Passport-country-desc-value">2 百万英镑</span>
+                                </li>
+                                <li>免签163个国家</li>
+                            </ul>
+                        </li>
+                        <li class="Passport-country-item">
+                            <a class="Passport-country-item-img" href="">
+                                <img src="../../assets/img/Passport/cyprus.jpg" alt="">
+                            </a>
+                            <a class="Passport-country-item-title" href="">塞浦路斯</a>
+                            <ul>
+                                <li>180天拿护照</li>
+                                <li>
+                                    <span class="Passport-country-desc-title">最低投资额</span>
+                                    &nbsp;-&nbsp;
+                                    <span class="Passport-country-desc-value">2百万欧元 (房产投资)</span>
+                                </li>
+                                <li>免签171个国家</li>
+                            </ul>
+                        </li>
+                        <li class="Passport-country-item">
+                            <a class="Passport-country-item-img" href="">
+                                <img src="../../assets/img/Passport/Malta.jpg" alt="">
+                            </a>
+                            <a class="Passport-country-item-title" href="">马耳他</a>
+                            <ul>
+                                <li>14 个月拿护照</li>
+                                <li>
+                                    <span class="Passport-country-desc-title">最低投资额</span>
+                                    &nbsp;-&nbsp;
+                                    <span class="Passport-country-desc-value">115万欧元</span>
+                                </li>
+                                <li>免签182个国家</li>
+                            </ul>
+                        </li>
+                        <li class="Passport-country-item">
+                            <a class="Passport-country-item-img" href="">
+                                <img src="../../assets/img/Passport/bjly.jpg" alt="">
+                            </a>
+                            <a class="Passport-country-item-title" href="">保加利亚</a>
+                            <ul>
+                                <li>2年内取得公民身份</li>
+                                <li>
+                                    <span class="Passport-country-desc-title">最低投资额</span>
+                                    &nbsp;-&nbsp;
+                                    <span class="Passport-country-desc-value">51.2万欧元</span>
+                                </li>
+                                <li>免签证旅行至169个国家</li>
+                            </ul>
+                        </li>
+                        <li class="Passport-country-item">
+                            <a class="Passport-country-item-img" href="">
+                                <img src="../../assets/img/Passport/hsghg.jpg" alt="">
+                            </a>
+                            <a class="Passport-country-item-title" href="">黑山共和国</a>
+                            <ul>
+                                <li>3个月内取得公民身份</li>
+                                <li>
+                                    <span class="Passport-country-desc-title">最低投资额</span>
+                                    &nbsp;-&nbsp;
+                                    <span class="Passport-country-desc-value">35万欧元</span>
+                                </li>
+                                <li>免签证旅行至122个国家</li>
+                            </ul>
+                        </li>
+                        <li class="Passport-country-item">
+                            <a class="Passport-country-item-img" href="">
+                                <img src="../../assets/img/Passport/teq.jpg" alt="">
+                            </a>
+                            <a class="Passport-country-item-title" href="">土耳其</a>
+                            <ul>
+                                <li>2个月拿护照</li>
+                                <li>
+                                    <span class="Passport-country-desc-title">最低投资额</span>
+                                    &nbsp;-&nbsp;
+                                    <span class="Passport-country-desc-value">25万美元</span>
+                                </li>
+                                <li>免签证旅行至115个国家</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="Passport-country">
+                    <h2>加勒比和瓦努阿图公民身份</h2>
+                    <ul class="Passport-country-list">
+                        <li class="Passport-country-item">
+                            <a class="Passport-country-item-img" href="">
+                                <img src="../../assets/img/Passport/Dominica.jpg" alt="">
+                            </a>
+                            <a class="Passport-country-item-title" href="">多米尼克</a>
+                            <ul>
+                                <li>2-3个月拿护照</li>
+                                <li>
+                                    <span class="Passport-country-desc-title">最低投资额</span>
+                                    &nbsp;-&nbsp;
+                                    <span class="Passport-country-desc-value">10万美元</span>
+                                </li>
+                                <li>免签127 个国家</li>
+                            </ul>
+                        </li>
+                        <li class="Passport-country-item">
+                            <a class="Passport-country-item-img" href="">
+                                <img src="../../assets/img/Passport/lgnd.jpg" alt="">
+                            </a>
+                            <a class="Passport-country-item-title" href="">林格纳达投资公民</a>
+                            <ul>
+                                <li>3-4个月拿护照</li>
+                                <li>
+                                    <span class="Passport-country-desc-title">最低投资额</span>
+                                    &nbsp;-&nbsp;
+                                    <span class="Passport-country-desc-value">15万美元</span>
+                                </li>
+                                <li>免签131 个国家</li>
+                            </ul>
+                        </li>
+                        <li class="Passport-country-item">
+                            <a class="Passport-country-item-img" href="">
+                                <img src="../../assets/img/Passport/slxy.jpg" alt="">
+                            </a>
+                            <a class="Passport-country-item-title" href="">圣卢西亚</a>
+                            <ul>
+                                <li>3-4个月拿护照</li>
+                                <li>
+                                    <span class="Passport-country-desc-title">最低投资额</span>
+                                    &nbsp;-&nbsp;
+                                    <span class="Passport-country-desc-value">10万美元</span>
+                                </li>
+                                <li>免签证旅行至132个国家</li>
+                            </ul>
+                        </li>
+                        <li class="Passport-country-item">
+                            <a class="Passport-country-item-img" href="">
+                                <img src="../../assets/img/Passport/atg.jpg" alt="">
+                            </a>
+                            <a class="Passport-country-item-title" href="">安提瓜和巴布达</a>
+                            <ul>
+                                <li>3（4）个月拿护照</li>
+                                <li>
+                                    <span class="Passport-country-desc-title">最低投资额</span>
+                                    &nbsp;-&nbsp;
+                                    <span class="Passport-country-desc-value">10万美元</span>
+                                </li>
+                                <li>免签证旅行至140个国家</li>
+                            </ul>
+                        </li>
+                        <li class="Passport-country-item">
+                            <a class="Passport-country-item-img" href="">
+                                <img src="../../assets/img/Passport/sklxsdf.jpg" alt="">
+                            </a>
+                            <a class="Passport-country-item-title" href="">圣克里斯多福和尼维斯</a>
+                            <ul>
+                                <li>46-60天拿护照</li>
+                                <li>
+                                    <span class="Passport-country-desc-title">最低投资额</span>
+                                    &nbsp;-&nbsp;
+                                    <span class="Passport-country-desc-value">15万美元</span>
+                                </li>
+                                <li>免签证旅行至152个国家</li>
+                            </ul>
+                        </li>
+                        <li class="Passport-country-item">
+                            <a class="Passport-country-item-img" href="">
+                                <img src="../../assets/img/Passport/Vanuatu.jpg" alt="">
+                            </a>
+                            <a class="Passport-country-item-title" href="">瓦努阿图</a>
+                            <ul>
+                                <li>1个月拿护照</li>
+                                <li>
+                                    <span class="Passport-country-desc-title">最低投资额</span>
+                                    &nbsp;-&nbsp;
+                                    <span class="Passport-country-desc-value">13万美元</span>
+                                </li>
+                                <li>免签证旅行至125个国家</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="page-container">
+            <div class="page-container-content  Passport-padding">
+                <div class="land-section Passport-country-list">
+                    <div class="Passport-title-content">
+                        <p>随着投资移民所带来的经济优势越来越为人们所熟知，越来越多的国家开始提供投资获取国籍的途径。投资公民计划为投资者提供合法的公民身份，允许他们获得第二种公民身份和护照，作为对该国经济投资的交换。</p>
+                        <p>&nbsp;</p>
+                        <p>根据国家的不同，投资价值从刚刚超过10万美元(安提瓜和巴布达，圣卢西亚和多米尼克)到超过250万欧元(塞浦路斯)不等。投资范围选择广泛，包括捐赠政府基金、房地产投资或商业投资。Astons值得信赖的顾问将帮助您选择最佳的选择和公民项目，并根据您的需求提供具有吸引力的投资组合选择。</p>
+                        <p>&nbsp;</p>
+                        <p>Astons为客户获得居住权和公民身份提供一站式服务。整个过程由行业专家精心管理。这一过程始于一次彻底的磋商，讨论潜在的投资路线，并寻找我们最好的房地产项目。它会一直持续到你准备并提交完整的最终申请。Astons一直在你身边工作，直到你成为全球公民的那一刻。</p>
+                    </div>
+                    <a class="title-btn" href="">请联系我</a>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+<script>
+    export default{
+
+    }
+</script>
+<style lang="css">
+    .Passport-padding{
+        padding-bottom: 8rem;
+        padding-top: 1rem;
+    }
+    .Passport-title{
+        margin-right: -1.5rem;
+        margin-left: -1.5rem;
+        display: flex;
+        justify-content: flex-start;
+        align-items: normal;
+        flex-flow: row wrap;
+        padding-bottom: 0;
+    }
+    .Passport-title-content{
+        padding-right: 1.5rem;
+        padding-left: 1.5rem;
+        width: 66.6666666667%;
+        margin-bottom: 0;
+    }
+    .Passport-title-content h1{
+        font-size: 5rem;
+        line-height: 5.5rem;
+        margin-bottom: 1.5rem;
+    }
+    .Passport-title-content p{
+        font-size: 1.8rem;
+        line-height: 2.7rem;
+    }
+    .Passport-country{
+        margin-bottom: 4rem;
+    }
+    .Passport-country h2{
+        font-size: 3.6rem;
+        line-height: 4.4rem;
+        border-top: 1px solid #E5E5E5;
+        padding: 4rem 0;
+    }
+    .Passport-country .Passport-country-list,
+    .land-section.Passport-country-list{
+        margin-right: -1.5rem;
+        margin-left: -1.5rem;
+        justify-content: flex-start;
+        align-items: flex-start;
+        flex-flow: row wrap;
+        display: flex;
+    }
+    .Passport-country-item{
+        width: 33.3333333333%;
+        padding-right: 1.5rem;
+        padding-left: 1.5rem;
+        margin-bottom: 6rem;
+    }
+    .Passport-country-item img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    .Passport-country-item .Passport-country-item-img{
+        display: block;
+        width: 100%;
+        margin-bottom: 1.5rem;
+        position: relative;
+        height: 200px;
+    }
+    .Passport-country-item .Passport-country-item-img:after {
+        content: '';
+        opacity: 0;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0,0,0,0.15);
+        transition: opacity 0.2s;
+    }
+    .Passport-country-item .Passport-country-item-title{
+        font-weight: bold;
+        display: block;
+        color: #000;
+        margin-bottom: 1rem;
+        max-height: 78px;
+        overflow: hidden;
+        font-size: 2.7rem;
+        line-height: 3.3rem;
+    }
+    .Passport-country-item .Passport-country-item-title:hover,
+    .Passport-country-item .Passport-country-item-title:active {
+        color: #E4002B;
+        text-decoration: none;
+    }
+    .Passport-country-item ul{
+        color: #333;
+    }
+    .Passport-country-desc-title{
+        font-weight: bold; 
+    }
+    .Passport-country-desc-value{
+        font-weight: bold;
+        color: #E4002B;
+    }
+</style>
